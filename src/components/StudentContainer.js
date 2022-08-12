3297f278789613856a2b3fab40fe0811181b6c34
mbuot22 <190408065@live.unilag.edu.ng>
2022-08-12 13:42:12 -0400

    first commit

---
+++ src/components/StudentContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+class StudentContainer extends React.Component{
+    render(){
+        return(
+            <>
+                <h1>Hello from Torilo world!</h1>
+                <p>I am the parent component.</p>
+            </>
+        )
+    }
+}
+export default StudentContainer;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import StudentContainer from './components/StudentContainer';
+
+// const element = <h1>Welcome to Torilo Student Management App!</h1>;
+ReactDOM.render(
+    <React.StrictMode>
+        <StudentContainer />
+    </React.StrictMode>,
+document.getElementById('root'));
